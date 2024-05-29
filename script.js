a3d88b65d66a337264303b03c58a66dc855bbd7e
andreihenrique <128196140+andreihenrique@users.noreply.github.com>
2024-05-29 23:46:47 +0000

    29/05/2024

---
+++ script.js
@@ -0,0 +1,48 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "resposta 1",
+      "resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 2",
+    alternativas:[
+      "resposta 1",
+      "resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 3",
+    alternativas:[
+      "resposta 1",
+      "resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 4",
+    alternativas:[
+      "resposta 1",
+      "resposta 2",
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 5",
+    alternativas:[
+      "resposta 1",
+      "resposta 2",
+    ]
+  }
+  
+];
